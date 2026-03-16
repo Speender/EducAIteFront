6b8627b60a58bae79d6d83a259fa6151fd101e8c
Zeth Brandino <vicbbbb2@proton.me>
2026-03-16 22:22:24 +0800

    landing pages components

--- Frontend/src/pages/main/components/LandingLoggedIn.tsx
+++ Frontend/src/pages/main/components/LandingLoggedIn.tsx
@@ -62,7 +62,7 @@ const aiInsights = [
 ]
 
 
-const Main: React.FC = () => (
+const LandingLoggedIn: React.FC = () => (
     <div className="max-w-7xl mx-auto p-6 flex flex-col gap-8">
 
         <div className="flex flex-col items-center gap-4">
@@ -98,4 +98,4 @@ const Main: React.FC = () => (
     </div>
 );
 
-export default Main
+export default LandingLoggedIn
